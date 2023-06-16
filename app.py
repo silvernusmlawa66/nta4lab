1d8ff35c3defa376b33025fc1ce392ea2577c398
silvernusmlawa66 <136784211+silvernusmlawa66@users.noreply.github.com>
2023-06-16 02:11:27 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import random
+
+st.little('Guess the number game')
+
+#generate a rondom number between 1 and 100
+number = random.randint(1,100)
+
+guss = st.number_input("enter a number(1 and 100):,min-value=1,max_value=100)
+                       
+if st.button('make a guess!'):
+    if guess > number:
+        st.write('Too high! try a smaller number.')
+    elief guess < number:
+        st.write('Too low! try a lager number.')
+    else:
+        st.write('congraturations! you have guessed the number correctly.')               
+                       
+          
